a978ab468c5506a345390ca43a1d8cfd1f296269
Ved Panchal <vedpanchal99@gmail.com>
2023-12-27 19:43:36 +0530

    database js update

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
 
 exports.connectDatabase = async() => {
-   await mongoose.connect(process.env.MONGO_URI)
+   await mongoose.connect(process.env.MONGO_URI,{
+    useNewUrlParser: true,
+      useUnifiedTopology: true,
+      connectTimeoutMS: 10000, // 10 seconds timeout
+      socketTimeoutMS: 45000,
+   })
     .then((con) => console.log(`Database Connected: ${con.connection.host}`))
     .catch((err) => console.log(err));
 }
